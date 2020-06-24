bca104a14a0a2c0b8a9d2ec89c9cf42c03cdb602
unknown <jaydeep0216@gmail.com>
2020-06-24 21:20:34 +0530

    Second JS File

--- arrow.js
+++ arrow.js
@@ -1,15 +1,3 @@
-let value = [10, 20, 30];
-let doubled = value.map((double) => double*2);
-console.log(doubled);
-export{doubled};
-
-const level = [20,50,30,10,40];
-let high = level.filter((n) =>{
-    return n < 30;
-})
-console.log(high);
-
-setTimeout(() => {
-    console.log(level);
-   
-}, 3000);
+const data = require("./new")
+console.log(data.value);
+console.log("result is", data);
--- new.js
+++ new.js
@@ -1,2 +1,16 @@
-import { 'double' } from './arrow';
+let value = [10, 20, 30];
+let doubled = value.map((double) => double*2);
 console.log(doubled);
+/*export{doubled};*/
+module.exports={
+    value
+};
+const level = [20,50,30,10,40];
+let high = level.filter((n) =>{
+    return n < 30;
+})
+console.log(high);
+setTimeout(() => {
+    console.log(level);
+   
+}, 3000);
